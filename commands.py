567e916f9015040045bb513512b30cced85c511a
Mohamed <AMOHAM89@jaguarlandrover.com>
2026-01-13 10:39:20 +0000

    updated commands.py

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
 from speech import speak
 from reminders import add_event, today_events
+from lists import add_to_category
 from llm import ask_llm
 import datetime
 import json
@@ -31,6 +32,7 @@ def handle_command(cmd):
             for e in events:
                 speak(f"{e['time']} {e['text']}")
         return "active"
+    
 
     else:
         response = ask_llm(f"""
@@ -38,6 +40,9 @@ def handle_command(cmd):
         Interpret the user's command below. 
         If the user wants to add an event, respond ONLY in this JSON format:
         {{ "action": "add_event", "day": "day_of_week", "time": "HH:MM", "text": "description" }}
+        If the user wants to something to a list, respond ONLY in this JSON format:
+        {{{ "action": "add_to_list", "category": "category_name", "item": "item_text" }}}                   
+                           
         If it is a general query or chat, respond normally in plain text.
         User command: "{cmd}"
         """)
@@ -47,9 +52,16 @@ def handle_command(cmd):
             if parsed.get("action") == "add_event":
                 add_event(parsed["day"], parsed["time"], parsed["text"])
                 speak(f"Event '{parsed['text']}' added for {parsed['day']} at {parsed['time']}")
+            elif parsed.get("action") == "add_to_list":
+                category = parsed["category"]
+                item = parsed["item"]
+
+                add_to_category(category, item)
+                speak(f"Added {item} to {category}")
+
             else:
                 speak(response)
         except json.JSONDecodeError:
-            speak(response)
+                speak(response)
 
     return "active"
