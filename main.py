cee91ed13df8177b90e2a8302a0f30447b7ee423
animo1738 <anis.mohamed2407@gmail.com>
2026-01-14 21:28:56 +0000

    Fix microphone device name handling

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ print("Jarvis started")
 
 def find_mic_index():
     devices = PvRecorder.get_available_devices()
+    name = devices.lower()
     for i, device in enumerate(devices):
         if ("usb" in name or "fifine" in name) and "monitor" not in name:
             print(f"Found Microphone at Index {i}: {device}")
