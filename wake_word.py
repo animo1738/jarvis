b53e4c4e49e050a480a1ef70d373206c7bc5545d
animo1738 <anis.mohamed2407@gmail.com>
2026-01-14 19:14:59 +0000

    Enhance wake word detection with additional keywords
    Updated the wake word listener to support multiple keywords.

--- wake_word.py
+++ wake_word.py
@@ -7,9 +7,18 @@ import struct
 
 #enables the jarvis to be constantly listening for wakeword
 def listen_wake_word():
-    porcupine = pvporcupine.create(keywords=["jarvis"])
+
+    #picovoice access key that allows us to listen to keywords
+    access_key = "9ta+47WGMP6Wb0szstPJ2D/0cZ8L5ev2wUjrcV3aBn+hzK33pZ6WYw=="
     #keywords to listen for 
     pa = pyaudio.PyAudio()
+    try:
+        porcupine = pvporcupine.create(
+            access_key=access_key, 
+            keywords=["jarvis", "hey jarvis", "wake up" ]
+        )
+    except Exception as e:
+        print(f"Error initializing Porcupine: {e}")
 
     stream = pa.open(
         rate=porcupine.sample_rate,
