ec6c100d61b04aa8b2063db7c5ca7d8778a73ae7
animo1738 <anis.mohamed2407@gmail.com>
2026-01-18 20:36:57 +0000

    Delete speech.py

--- speech.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-#uses linux built in text to speech engine to produce spoken audio, basis of our jarvis talking back to us
-
-engine = pyttsx3.init()
-engine.setProperty("rate", 165)
-
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
